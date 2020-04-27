609d844d6f3ccdc2b04d4ff4e5b8baced31a2aab
Skipp-it <mihairaducroitoru@gmail.com>
2020-04-27 11:39:28 +0300

    final

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,5 @@
-from typing import List, Dict
+import typing
+
 
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
@@ -19,8 +20,151 @@ def get_mentors(cursor: RealDictCursor) -> list:
 @database_common.connection_handler
 def get_mentors_by_last_name(cursor: RealDictCursor, last_name: str) -> list:
     query = """
-        SELECT first_name, last_name, city
-        FROM mentor
-        ORDER BY first_name"""
+            SELECT first_name, last_name, city
+            FROM mentor
+            WHERE last_name
+            LIKE %s
+            ORDER BY first_name
+            """
+    args = last_name
+    cursor.execute(query, (args,))
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_mentors_by_city(cursor: RealDictCursor, city_name: str) -> list:
+    query = f"""
+            SELECT first_name, last_name, city
+            FROM mentor
+            WHERE city
+            LIKE '{city_name}'
+            ORDER BY first_name
+            """
     cursor.execute(query)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_applicant_data_by_name(cursor: RealDictCursor, first_name: str) -> list:
+    query = """
+                SELECT first_name, last_name, phone_number
+                FROM applicant
+                WHERE first_name 
+                LIKE %s 
+                OR last_name LIKE %s
+                ORDER BY last_name
+                """
+    args = first_name, first_name
+    cursor.execute(query, args)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_applicant_data_by_email(cursor: RealDictCursor, email_ending: str) -> list:
+    query = """
+                SELECT first_name, last_name, phone_number
+                FROM applicant
+                WHERE email 
+                LIKE %s 
+                """
+    args = '%' + email_ending
+    cursor.execute(query, (args,))
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_applicant(cursor: RealDictCursor) -> list:
+    query = """
+                SELECT *
+                FROM applicant
+                ORDER BY first_name
+                """
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_applicant_by_code(cursor: RealDictCursor, code: str) -> list:
+    query = """
+            SELECT *
+            FROM applicant
+            WHERE application_code = %s
+            """
+    args = code
+    cursor.execute(query, (args,))
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def update_phone_number(cursor: RealDictCursor, phone_no: str, code: str) -> list:
+    query = """
+            UPDATE applicant
+            SET phone_number = %s
+            WHERE application_code = %s
+            """
+    args = phone_no, code
+    cursor.execute(query, args)
+    updated_query = """
+            SELECT *
+            FROM applicant
+            WHERE application_code = %s
+            """
+    args = code
+    cursor.execute(updated_query, (args,))
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def delete_applicant(cursor: RealDictCursor, code: str) -> list:
+    query= """
+            DELETE FROM applicant
+            WHERE application_code = %s
+            """
+    args = code
+    cursor.execute(query, (args,))
+    return "Done"
+
+
+@database_common.connection_handler
+def add_applicant(cursor: RealDictCursor, first_name: str, last_name: str, phone_number: str, email: str) -> list:
+    query = """
+            INSERT INTO applicant (first_name, last_name, phone_number, email)
+            VALUES (%s,%s,%s,%s)   
+            """
+    args = first_name, last_name, phone_number, email
+    cursor.execute(query, args)
+    return "Done"
+
+
+@database_common.connection_handler
+def delete_applicant_by_email(cursor: RealDictCursor, email_domain: str) -> list:
+    query= """
+            DELETE FROM applicant
+            WHERE email 
+            LIKE %s
+            """
+    args = '%' + email_domain
+    cursor.execute(query, (args,))
+    return "Done"
+
+
+@database_common.connection_handler
+def get_max_id(cursor: RealDictCursor) -> list:
+    query = """
+            SELECT MAX(id)
+            FROM applicant
+            """
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_application_code_by_id(cursor: RealDictCursor, applicant_id: int) -> list:
+    query = """
+            SELECT application_code
+            FROM applicant
+            WHERE id = %s 
+            """
+    args = applicant_id
+    cursor.execute(query, (args,))
     return cursor.fetchall()
--- main.py
+++ main.py
@@ -13,15 +13,76 @@ def index():
 @app.route('/mentors')
 def mentors_list():
     mentor_name = request.args.get('mentor-last-name')
-
+    city_name = request.args.get('city-name')
     if mentor_name:
         mentor_details = data_manager.get_mentors_by_last_name(mentor_name)
+    elif city_name:
+        mentor_details = data_manager.get_mentors_by_city(city_name)
     else:
         mentor_details = data_manager.get_mentors()
 
     # We get back a list of dictionaries from the data_manager (for details check 'data_manager.py')
+    return render_template('mentors.html', mentors=mentor_details, city_name=city_name) #     city_name are rostul, aici, de a pastra ultima selectie pe buton
 
-    return render_template('mentors.html', mentors=mentor_details)
+
+@app.route('/applicants-phone')
+def applicants_phone():
+    applicant_name = request.args.get('applicant-name')
+    applicant_email = request.args.get('email-ending')
+    if applicant_name:
+        applicant_details = data_manager.get_applicant_data_by_name(applicant_name)
+    elif applicant_email:
+        applicant_details = data_manager.get_applicant_data_by_email(applicant_email)
+    else:
+        applicant_details = ""
+    return render_template('applicant-phone.html', applicants=applicant_details)
+
+
+@app.route('/applicants')
+def applicants_list():
+    applicants_details = data_manager.get_applicant()
+    return render_template('applicants.html', applicants=applicants_details)
+
+
+@app.route('/applicants/<code>', methods=['GET','POST'])
+def applicant_code(code):
+    if request.method == 'POST':
+        phone_no = request.form['new-phone']
+        data_manager.update_phone_number(phone_no, code)
+        return redirect('/applicants/' + code)
+    else:
+        applicant_details = data_manager.get_applicant_by_code(code)
+    return render_template('applicants-update.html', applicants=applicant_details, code=code)
+
+
+@app.route('/applicants/<code>/delete')
+def delete_applicant(code):
+    data_manager.delete_applicant(code)
+    return redirect('/applicants')
+
+
+@app.route('/add-applicant', methods=['GET','POST'])
+def add_applicant():
+    if request.method == 'POST':
+        first_name = request.form['first-name']
+        last_name = request.form['last-name']
+        phone_number = request.form['phone-number']
+        email = request.form['email']
+        data_manager.add_applicant(first_name, last_name, phone_number, email)
+        code = data_manager.get_max_id()
+        code_data = code[0].get('max','')
+        app_code = data_manager.get_application_code_by_id(code_data)
+        applicant_code = app_code[0].get('application_code', '')
+        return redirect('/applicants/' + str(applicant_code))
+    return render_template('add-applicant.html')
+
+
+@app.route("/applicants/delete-by-email", methods=['GET','POST'])
+def delete_by_email():
+    if request.method == 'POST':
+        email_domain = request.form['email-ending']
+        data_manager.delete_applicant_by_email(email_domain)
+        return redirect('/applicants')
 
 
 if __name__ == '__main__':
